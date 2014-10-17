14220c3c285f0f0e0ca977390138ccce2acdb6a7
irthos <irth03@gmail.com>
2014-10-16 21:57:54 -0700

    prerender and heroku stuffs

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
-app.use(require('prerender-node').set('prerenderToken', 'oDbfSC56YdKizTprVuCo'));
+app.use(require('prerender-node').set('prerenderToken', 'ipMxPbev4DV5j1W9P7Mq'));
 //var prerender = require('prerender-node').set('prerenderServiceUrl', 'http://blooming-brushlands-2690.herokuapp.com/');
 //app.use(prerender);
 app.use(express.logger('dev'));
